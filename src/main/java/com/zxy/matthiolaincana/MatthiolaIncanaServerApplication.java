a5b7a7a4d9757a93cc2de640556259f34e67844c
xiangyong.zeng <zengxiangyong@xcxd.com>
2020-04-27 17:17:58 +0800

    Initial commit

---
+++ src/main/java/com/zxy/matthiolaincana/MatthiolaIncanaServerApplication.java
@@ -0,0 +1,13 @@
+package com.zxy.matthiolaincana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatthiolaIncanaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatthiolaIncanaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zxy/matthiolaincana/demo/MatthiolaIncanaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxy.matthiolaincana.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatthiolaIncanaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
