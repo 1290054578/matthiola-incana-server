00b9117aea2f8d07380033006a0abafbb68606be
xiangyong.zeng <zengxiangyong@xcxd.com>
2020-05-15 17:41:41 +0800

    .

--- src/main/java/com/zxy/matthiolaincana/MatthiolaIncanaServerApplication.java
+++ src/main/java/com/zxy/matthiolaincana/MatthiolaIncanaServerApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MatthiolaIncanaServerApplication {
 
-	public static void main(String[] args) {
-
-
-		SpringApplication.run(MatthiolaIncanaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MatthiolaIncanaServerApplication.class, args);
+    }
 
 }
