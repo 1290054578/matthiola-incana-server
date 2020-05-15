688d47c53947d3a07fb17565329a75542eb3d404
xiangyong.zeng <zengxiangyong@xcxd.com>
2020-05-15 16:09:13 +0800

    .

--- src/test/java/com/zxy/matthiolaincana/demo/MatthiolaIncanaServerApplicationTests.java
+++ src/test/java/com/zxy/matthiolaincana/demo/MatthiolaIncanaServerApplicationTests.java
@@ -1,14 +1,16 @@
 package com.zxy.matthiolaincana.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class MatthiolaIncanaServerApplicationTests {
 
     @Test
     void contextLoads() {
-
+        log.info("我在测试打印日志，具体打印位置不知道");
     }
 
 }
